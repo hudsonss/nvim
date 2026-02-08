26060c2e316428480fc159a6eb7d9842b89c8774
hudsonss <nosduh.blade@gmail.com>
2026-02-08 13:23:30 -0400

    feat: Add initial LSP configuration with Mason and nvim-lspconfig, including setup for Lua and Python language servers.

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -7,49 +7,56 @@
 return {
     {
         "williamboman/mason.nvim",
-        cmd = "Mason",
-        keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
-        build = ":MasonUpdate",
         config = function()
             require("mason").setup()
         end,
     },
     {
         "williamboman/mason-lspconfig.nvim",
+        dependencies = { "williamboman/mason.nvim" },
         config = function()
             require("mason-lspconfig").setup({
                 ensure_installed = { "lua_ls", "pyright" },
+                automatic_installation = true,
             })
         end,
     },
     {
         "neovim/nvim-lspconfig",
-        tag = "v0.1.8", -- Pinning version to avoid breaking changes in master/nightly
         dependencies = {
-            "williamboman/mason.nvim",
             "williamboman/mason-lspconfig.nvim",
             "hrsh7th/cmp-nvim-lsp",
         },
         config = function()
-            -- Adiciona capacidades de autocompletar ao LSP padrão
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            local lspconfig = require("lspconfig")
+            local mason_lspconfig = require("mason-lspconfig")
 
-            -- Configuração para Python (Pyright)
-            lspconfig.pyright.setup({
-                capabilities = capabilities,
-            })
+            -- Configuração automática via Mason Handlers
+            -- Isso garante que todos os servidores instalados pelo Mason sejam configurados corretamente
+            mason_lspconfig.setup_handlers({
+                -- Handler padrão (para todos os servidores sem configuração específica)
+                function(server_name)
+                    require("lspconfig")[server_name].setup({
+                        capabilities = capabilities,
+                    })
+                end,
 
-            -- Configuração para Lua (Lua LS)
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-                settings = {
-                    Lua = {
-                        diagnostics = {
-                            globals = { "vim" },
+                -- Configuração específica para Lua
+                ["lua_ls"] = function()
+                    require("lspconfig").lua_ls.setup({
+                        capabilities = capabilities,
+                        settings = {
+                            Lua = {
+                                diagnostics = {
+                                    globals = { "vim" },
+                                },
+                            },
                         },
-                    },
-                },
+                    })
+                end,
+                
+                -- Se precisar de config específica para Python, adicione aqui:
+                -- ["pyright"] = function() ... end
             })
         end,
     },
