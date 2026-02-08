606c6511a809e2f7dfb7aa4c3bb8b090c79fb40c
hudsonss <nosduh.blade@gmail.com>
2026-02-08 13:40:18 -0400

    feat: Add nvim-treesitter configuration with installed parsers, highlighting, and indentation.

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,24 +1,64 @@
 --[[
   Treesitter
-  Motor de destaque de sintaxe (syntax highlighting) avançado.
-  Melhora significativamente as cores do código, entendendo a estrutura real da linguagem.
+  Destaque de sintaxe baseado em árvore sintática (AST)
+  Configuração clássica e compatível com lazy.nvim
 ]]
+
 return {
     "nvim-treesitter/nvim-treesitter",
-    version = false, -- last release is way too old and doesn't work on Windows
+
+    -- Não fixa versão: releases antigas costumam causar problemas
+    version = false,
+
+    -- Atualiza os parsers após instalação/atualização
     build = ":TSUpdate",
+
+    -- Carrega apenas quando um arquivo for aberto/criado
     event = { "BufReadPost", "BufNewFile" },
+
     dependencies = {
         "nvim-treesitter/nvim-treesitter-textobjects",
     },
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-                "c", "lua", "vim", "vimdoc", "python", "javascript", "html", "java", "query", "markdown", "markdown_inline"
-            },
-            sync_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end,
+
+    ------------------------------------------------------------------
+    -- opts é aplicado automaticamente como:
+    -- require("nvim-treesitter.configs").setup(opts)
+    ------------------------------------------------------------------
+    opts = {
+        ------------------------------------------------------------------
+        -- Parsers que devem estar instalados
+        ------------------------------------------------------------------
+        ensure_installed = {
+            "c",
+            "lua",
+            "vim",
+            "vimdoc",
+            "python",
+            "javascript",
+            "html",
+            "java",
+            "query",
+            "markdown",
+            "markdown_inline",
+        },
+
+        ------------------------------------------------------------------
+        -- Instalação assíncrona (não bloqueia o editor)
+        ------------------------------------------------------------------
+        sync_install = false,
+
+        ------------------------------------------------------------------
+        -- Destaque de sintaxe via Treesitter
+        ------------------------------------------------------------------
+        highlight = {
+            enable = true,
+        },
+
+        ------------------------------------------------------------------
+        -- Indentação baseada na árvore sintática
+        ------------------------------------------------------------------
+        indent = {
+            enable = true,
+        },
+    },
 }
